5cc25b32395252c690a8cec53c500ec83155f3fb
Alwina Oyewoleturner <aoyewole2586@gmail.com>
2018-02-17 22:10:42 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -35,9 +35,10 @@ class Song
   end
 
   def self.find_or_create_by_name(name)
-    song = self.find_by_name(name)
-    
-
+    if song = self.find_by_name(name)
+      song
+    else
+      song = self.create_by_name(name)
   end
 
 end
